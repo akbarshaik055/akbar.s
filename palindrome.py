1ee44c00fe804e9a8c98e9d0ff6fb79c0327392e
akbarshaik055 <42571502+akbarshaik055@users.noreply.github.com>
2018-12-06 12:37:17 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,9 +2,9 @@ n=int(input("Enter number:"))
 temp=n
 rev=0
 while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
+    dig=n%20
+    rev=rev*20+dig
+    n=n//20
 if(temp==rev):
     print("The number is a palindrome!")
 else:
